71477d81537f1454cadb62017660bf9f047b82f5
Replit user <>
2023-04-20 03:45:38 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -223,7 +223,7 @@ Pair * nextTreeMap(TreeMap * tree) {
         }
         current=current->parent;    
     }
-    return (tree->current != NULL) ? tree->current->pair : NULL;
+    return NULL;
 }
 /*
 
