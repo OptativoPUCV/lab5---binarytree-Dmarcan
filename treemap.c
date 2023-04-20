80ca7d428f0f70acd1840b98b050aba874268bed
Replit user <>
2023-04-20 03:07:31 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -194,7 +194,7 @@ Pair * nextTreeMap(TreeMap * tree) {
     TreeNode * current=tree->current;
     while (current!=NULL)
     {
-        if (current->parent!=NULL && current->parent->pair->key>tree->current->pair->key)
+        if (current->parent!=NULL )
         {
             tree->current=current->parent;
             return current->parent->pair;
