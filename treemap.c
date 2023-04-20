3976acefdef87a168c0d29b68e7d17d81637c2f0
Replit user <>
2023-04-20 02:38:56 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -158,9 +158,30 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 Pair * upperBound(TreeMap * tree, void* key) {
     return NULL;
 }
+/*
+Implemente las funciones para recorrer la estructura: Pair* firstTreeMap(TreeMap* tree) retorna el primer **Pair** del mapa (el menor). Pair* nextTreeMap(TreeMap* tree)  retornar el siguiente **Pair** del mapa a partir del puntero TreeNode* current. Recuerde actualizar este puntero.
+
+    Pair * firstTreeMap(TreeMap * tree) {
+        
+
+        return NULL;
+    }
+
+    Pair * nextTreeMap(TreeMap * tree) {
+       
+
+        return NULL;
+    }
+*/
 
 Pair * firstTreeMap(TreeMap * tree) {
-    return NULL;
+    if (tree->root==NULL)return NULL;
+    TreeNode * current=tree->root;
+    while (current->left!=NULL)
+    {
+        current=current->left; 
+    }
+    return current->pair;
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
