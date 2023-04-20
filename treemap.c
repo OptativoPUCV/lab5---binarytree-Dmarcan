b51e5f7938aa051f58969dccb2300a32eeed30f3
Replit user <>
2023-04-20 01:23:35 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -97,7 +97,7 @@ TreeNode * minimum(TreeNode * x){
         if (current->left==NULL)return current;
         current=current->left;
     }
-    return current;
+    return current;//porque no me deja sacar este return?
 }
 
 
